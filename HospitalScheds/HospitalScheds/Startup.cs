1b4bc9537c5193b726b8c4ce9956bf2ec72bf54d
gaopengfei123166 <1810453372@qq.com>
2019-09-02 16:22:30 +0800

    注释

--- HospitalScheds/HospitalScheds/Startup.cs
+++ HospitalScheds/HospitalScheds/Startup.cs
@@ -19,7 +19,7 @@ namespace HospitalScheds
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        public IConfiguration Configuration { get; } //注释
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
