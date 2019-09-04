f51353aad0423e2b17e4ef7dfa28ededff1458cf
chuyuhang <273722343@qq.com>
2019-09-04 21:06:16 +0800

    44

---
+++ HospitalScheds/HospitalScheds.IServerce/ISurgeryForServerce.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using HospitalScheds.Model;
+
+namespace HospitalScheds.IServerce
+{
+    /// <summary>
+    /// 手术申请表
+    /// </summary>
+    public interface ISurgeryForServerce
+    {
+            /// <summary>
+            /// 显示
+            /// </summary>
+            /// <returns></returns>
+            List<SurgeryFor> GetSurgeryFor();
+
+            /// <summary>
+            /// 添加
+            /// </summary>
+            /// <param name="crewscheduling"></param>
+            /// <returns></returns>
+            int AddSurgeryFor(SurgeryFor surgeryFor);
+
+            /// <summary>
+            /// 修改
+            /// </summary>
+            /// <param name="crewscheduling"></param>
+            /// <returns></returns>
+            int EditSurgeryFor(SurgeryFor surgeryFor);
+
+        /// <summary>
+        /// 反填
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        SurgeryFor GetModel(int id);
+
+            /// <summary>
+            /// 删除
+            /// </summary>
+            /// <param name="id"></param>
+            /// <returns></returns>
+            int DeleteSurgeryFor(int id);
+        }
+}
