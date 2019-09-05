037cc14c8ac8962b233a2cce2cacc86a9f4815d9
suoyuting <2354893700@qq.com>
2019-09-05 11:48:16 +0800

    表

--- HospitalScheds/HospitalScheds.Serverce/DataContext.cs
+++ HospitalScheds/HospitalScheds.Serverce/DataContext.cs
@@ -19,6 +19,60 @@ namespace HospitalScheds.Serverce
 
         public DbSet<UserInfo> UserInfolist { get; set; }
 
+        /// <summary>
+        /// 消息公布
+        /// </summary>
+        public DbSet<Announcementform> Announcementform { get; set; }
+
+        /// <summary>
+        /// 年假申请设置
+        /// </summary>
+        public DbSet<AnnualVacation> AnnualVacation { get; set; }
+
+        /// <summary>
+        /// 审批表
+        /// </summary>
+        public DbSet<Approval> Approval { get; set; }
+
+        /// <summary>
+        /// 审批活动表
+        /// </summary>
+        public DbSet<Approvalactivity> Approvalactivity { get; set; }
+
+        /// <summary>
+        /// 审批条件
+        /// </summary>
+        public DbSet<ApprovalCriteria> ApprovalCriteria { get; set; }
+
+        /// <summary>
+        /// 考勤统计
+        /// </summary>
+        public DbSet<Attendance> Attendance { get; set; }
+
+        /// <summary>
+        /// 考勤统计明细表
+        /// </summary>
+        public DbSet<Attendancestatisticslist> Attendancestatisticslist { get; set; }
+
+        /// <summary>
+        /// 考勤统计汇总
+        /// </summary>
+        public DbSet<Attendancestatisticssummary> Attendancestatisticssummary { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public DbSet<BaseEntity> BaseEntity { get; set; }
+
+        /// <summary>
+        /// 人员排班
+        /// </summary>
+        public DbSet<Crewscheduling> Crewscheduling { get; set; }
+   
+
+
+
+
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
