b08541e6c382fc622111e2610fbeed536b290109
hanbo1904 <hanbo_66@163.com>
2019-09-05 10:46:06 +0800

    EF

---
+++ HospitalScheds/HospitalScheds.Model/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HospitalScheds.Model
+{
+    public abstract class BaseEntity
+    {
+
+    }
+}
---
+++ HospitalScheds/HospitalScheds.Serverce/DataContext.cs
@@ -0,0 +1,35 @@
+﻿using HospitalScheds.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HospitalScheds.Serverce
+{
+    public class DataContext: DbContext
+    {
+        public DataContext()
+        {
+
+        }
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<UserInfo> UserInfolist { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseMySQL("Data Source=169.254.139.102;Initial Catalog=projectmanage;User ID=root;pwd=123456");
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
