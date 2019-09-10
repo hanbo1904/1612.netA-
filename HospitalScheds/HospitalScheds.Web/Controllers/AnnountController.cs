05833d59ca3c5b7eb2a59557fcfd05eec1604504
hanbo1904 <hanbo_66@163.com>
2019-09-10 15:54:31 +0800

    显示

--- HospitalScheds/HospitalScheds.Web/Controllers/AnnountController.cs
+++ HospitalScheds/HospitalScheds.Web/Controllers/AnnountController.cs
@@ -14,12 +14,19 @@ namespace HospitalScheds.Web.Controllers
 {
     public class AnnountController : Controller
     {
-        
+
+
+        //public JsonResult Indexs(string Name = "")
+        //{
+        //    //string pagelist = Newtonsoft.Json.JsonConvert.SerializeObject(Name);
+        //    var result = HelperHttpClient.GetAll("get", "/api/annount",Name);
+        //    return Json(result);
+        //} 
         public IActionResult Index()
-        {
+        {     
             return View();
         }
-
+     
 
         public JsonResult Adds(Announcementform announcementform)   
         {
@@ -38,6 +45,13 @@ namespace HospitalScheds.Web.Controllers
         {
             return View();
         }
+        //public JsonResult Indexs(string Name = "", int pageIndex = 0, int pageSize = 3)
+        //{
+        //    string jsonm = Newtonsoft.Json.JsonConvert.SerializeObject(Name, pageIndex, pageSize);
+
+        //}
+
+             
         
     }
 }
