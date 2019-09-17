2cbdd6784a011652633ffe34f9fed93798f3328e
hanbo1904 <hanbo_66@163.com>
2019-09-17 20:01:36 +0800

    111

--- HospitalScheds/HospitalScheds/Controllers/AnnountController.cs
+++ HospitalScheds/HospitalScheds/Controllers/AnnountController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using HospitalScheds.IServerce;
+
 using HospitalScheds.Model;
 
 using Microsoft.AspNetCore.Http;
